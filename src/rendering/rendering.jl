8c8697497a7229cbd90c55ecfca14c223fc7a349
Shuhei Kadowaki <aviatesk@gmail.com>
2020-06-01 22:57:54 +0900

    LTS compat again and again

--- src/rendering/rendering.jl
+++ src/rendering/rendering.jl
@@ -29,6 +29,49 @@ function render_doc(doc)
     return render_doc(docformat, body, doc)
 end
 
+@static VERSION < v"1.1" && begin
+macro kwdef(expr)
+    expr = macroexpand(__module__, expr) # to expand @static
+    expr isa Expr && expr.head === :struct || error("Invalid usage of @kwdef")
+    T = expr.args[2]
+    if T isa Expr && T.head === :<:
+        T = T.args[1]
+    end
+
+    params_ex = Expr(:parameters)
+    call_args = Any[]
+
+    Base._kwdef!(expr.args[3], params_ex.args, call_args)
+    # Only define a constructor if the type has fields, otherwise we'll get a stack
+    # overflow on construction
+    if !isempty(params_ex.args)
+        if T isa Symbol
+            kwdefs = :(($(esc(T)))($params_ex) = ($(esc(T)))($(call_args...)))
+        elseif T isa Expr && T.head === :curly
+            # if T == S{A<:AA,B<:BB}, define two methods
+            #   S(...) = ...
+            #   S{A,B}(...) where {A<:AA,B<:BB} = ...
+            S = T.args[1]
+            P = T.args[2:end]
+            Q = [U isa Expr && U.head === :<: ? U.args[1] : U for U in P]
+            SQ = :($S{$(Q...)})
+            kwdefs = quote
+                ($(esc(S)))($params_ex) =($(esc(S)))($(call_args...))
+                ($(esc(SQ)))($params_ex) where {$(esc.(P)...)} =
+                    ($(esc(SQ)))($(call_args...))
+            end
+        else
+            error("Invalid usage of @kwdef")
+        end
+    else
+        kwdefs = nothing
+    end
+    quote
+        Base.@__doc__($(esc(expr)))
+        $kwdefs
+    end
+end
+end
 
 include("common.jl")
 include("htmlformats.jl")
